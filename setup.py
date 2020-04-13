69fed3272d564e1cd0504ee116273923a16d04f4
russlooker <russell@looker.com>
2020-04-13 00:06:41 -0700

    minor / whitespace

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lookml",
-    version="0.0.3a",
+    version="2.0.0",
     author="Russell Garner",
     author_email="russelljgarner@gmail.com",
     description="A pythonic api for programatically manipulating LookML",
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/llooker/lookml",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "lkml >= 0.2.2",
+        "PyGithub >= 1.47"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
